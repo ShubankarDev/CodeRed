3b0f35ea6543bf66ee531229c9c2f5ac102e8b39
C S MANODHITHYAA <manodhithyaa.cs@gmail.com>
2025-07-29 10:03:34 +0530

    changes done

---
+++ src/components/admin/admin.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+function Admin() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default Admin;
